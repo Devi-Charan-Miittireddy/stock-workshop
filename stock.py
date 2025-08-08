0c10471ddd1590e423e1da8831262c933fb28858
Devi-Charan-Mittireddy <charanmittireddy@gmail.com>
2025-08-08 22:20:02 +0530

    stock.py

--- stock.py
+++ stock.py
@@ -15,7 +15,7 @@ EMAIL_PASSWORD = "pavankumar123"
 
 # --------- FIREBASE SETUP ---------
 if not firebase_admin._apps:
-    cred = credentials.Certificate("stockmarket.json")  # path to your key
+    cred = credentials.Certificate("stockmarket-ws-be7ae-firebase-adminsdk-fbsvc-714919cf7a.json")  # path to your key
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
